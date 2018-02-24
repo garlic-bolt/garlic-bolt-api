91976f28394616a9482e2759f4a16f7850eb8519
baolinet <bao.li.net@gmail.com>
2018-02-24 10:45:50 +0800

    文件清理

---
+++ src/main/java/com/chanjetpay/garlic/result/BasicResult.java
@@ -0,0 +1,24 @@
+package com.chanjetpay.result;
+
+/**
+ * 无返回值结果
+ * @author 李宝
+ *
+ */
+public class BasicResult extends Result{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BasicResult(){
+		this.setCode(Result.SUCCESS);
+	}
+
+	public static BasicResult newBasicResult() {
+		return new BasicResult();
+	}
+
+	@Override
+	public boolean isError() {
+		return !Result.SUCCESS.equals(this.getCode());
+	}
+}
